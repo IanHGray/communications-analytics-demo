9e953b67bf1db354fdc340c05aeec93eef5dbe67
iangray <ian.gray@fticonsulting.com>
2023-07-14 16:16:01 -0500

    Removed network feature for Streamlit Community deployment. Lack of available resources crashes the page.

--- app/pages/4 - Visualize Language Similarity.py
+++ app/pages/4 - Visualize Language Similarity.py
@@ -13,11 +13,10 @@ try:
     This stage of analysis is optional, but can be helpful in certain situations--particularly when looking for
     memetic content or the prevelance of specific talking points.
 
-    Network visualizations are highly processor-intensive to render, and due to the limited resources available on this community server, this stage may take some time. For this reason, there are two available options here:
-    1.) Render a random sample of 1000 posts from the dataset on this page. 
-    2.) Generate the entire network as a file (.gexf), which can then be rendered locally with a network visualization tool like Gephi or Cytoscape.
+    Unfortunately, network visualizations are highly processor-intensive to render, and due to the limited resources available on this community server, this stage of analysis will not run without crashing.
 
-    """)
+    If you would like to run this analysis yourself, please download this repo and deploy locally in Docker.""")
+
 
     def create_network(minimum_similarity):
         pipeline.minimum_similarity = minimum_similarity
@@ -37,6 +36,7 @@ try:
             pipeline.save_network()
         return pipeline
 
+    """
     with st.form("run network"):
         col1, col2, col3 = st.columns(3)
         with col1:
@@ -45,7 +45,7 @@ try:
             submitted_2 = st.form_submit_button("Prep Full Dataset for Download")
         with col3:
             with st.expander("Advanced Options"):
-                st.markdown("""The slider below sets the minimum similarity between two documents required for them to be "linked" in the network. To cut down on processing time, I have limited these options to relatively high similarity, however different situations call for different settings.""")
+                st.markdown("The slider below sets the minimum similarity between two documents required for them to be "linked" in the network. To cut down on processing time, I have limited these options to relatively high similarity, however different situations call for different settings.")
                 minimum_similarity = st.slider("Minimum Similarity for Linkage", min_value = 0.3, max_value =0.99, value = 0.45)
 
     if submitted_1:
@@ -59,6 +59,8 @@ try:
         with open("similarity_analysis.gexf", 'r') as file:
             st.download_button("Download Results", data=file, file_name = 'similarity_results.gexf')
 
+    """
     st.session_state['pipeline'] = pipeline
+    
 except:
     st.markdown("""The topic model has not been run yet! Please return to the "Run Topic Model" page to proceed with the similarity analysis.""")
